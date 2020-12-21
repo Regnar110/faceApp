f3a522cd183d008c85a5ab5799a4bc6135f1e1d0
unknown <mateuszwrycza3@gmail.com>
2020-12-21 15:47:52 +0100

    url fix#2

--- smart-brain/src/components/topTen/Topten.js
+++ smart-brain/src/components/topTen/Topten.js
@@ -10,7 +10,7 @@ class Topten extends Component {
     }
 
     componentDidMount() {
-        fetch("https://hidden-mesa-50173.herokuapp.com//top", { 
+        fetch("https://hidden-mesa-50173.herokuapp.com/top", { 
             method: 'GET' // definiujemy jakiej metody użyjemy. Po stronie serwera też jest app.get więc tutaj musi być to samo.
         })
         .then(response => response.json())
